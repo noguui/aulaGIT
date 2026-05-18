c3486ee34b1ce1ef19d1027bdad2f5849bb16951
Noguixz7 <noguilhermek@gmail.com>
2026-05-18 08:28:18 -0300

    ultima vez eu acho

--- README.py
+++ README.py
@@ -1,8 +1,2 @@
 #aulaGIT
 #aula para integrar o GitHub
-#hoje é dia 13
-#hoje é dia 27
-#labubu
-#hoje é dia 04
-print("hello mundo estranho")
-print("estranho mesmo")
---
+++ teste aqui.py
@@ -0,0 +1,7 @@
+#hoje é dia 13
+#hoje é dia 27
+#labubu
+#hoje é dia 04
+print("hello mundo estranho")
+print("estranho mesmo")
+#ultima vez aqui? 18/05
